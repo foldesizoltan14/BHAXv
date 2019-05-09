ed8612a36816e3db1412385dcc3ce06d0344df39
foldesizoltan14 <43464693+foldesizoltan14@users.noreply.github.com>
2019-05-09 22:06:11 +0200

    Add files via upload

---
+++ Chomsky/Turing.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+int sz=0;
+int a=0;
+printf("Adj meg egy számot");
+scanf("%d", &sz);
+printf("Unarisba atvaltva:");
+for(int i=0;i<sz;i++)
+{
+printf("%d",1);
+       }
+
+return 0;
+}
---
+++ conway/Sejtauto1/main.cpp
@@ -0,0 +1,10 @@
+#include "sejtablak.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    SejtAblak w;
+    w.show();
+
+    return a.exec();
+}
